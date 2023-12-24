3e68dd8fc8563f981e79086f5dc4a950867f2d8f
Milan Hauth <milahu@gmail.com>
2023-12-24 12:24:46 +0100

    js: test jobserver-style: pipe fifo

--- js/test/jobclient/test.sh
+++ js/test/jobclient/test.sh
@@ -5,6 +5,16 @@ set -e
 cd "$(dirname "$0")"
 set -x
 
+make_exe=$(which make)
+
+for jobserver_style in pipe fifo; do
+
+echo testing "make --jobserver-style=$jobserver_style"
+
+function make() {
+  $make_exe --jobserver-style=$jobserver_style "$@"
+}
+
 make -j1 # 0 workers -> no jobserver
 
 make -j2 # 1 worker
@@ -14,5 +24,7 @@ make -j3 # 2 workers
 make -j4 # 3 workers
 
 make -j4 -l4 # 3 workers + maxLoad 4
+
+done
 
 echo all tests passed
