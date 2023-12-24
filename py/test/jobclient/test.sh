d78ef58d14c9f22221c4a7e19caefd759d671e10
Milan Hauth <milahu@gmail.com>
2023-12-24 20:19:54 +0100

    py: test jobserver-style: pipe fifo

--- py/test/jobclient/test.sh
+++ py/test/jobclient/test.sh
@@ -7,6 +7,16 @@ set -e
 cd "$d"
 set -x
 
+make_exe=$(which make)
+
+for jobserver_style in pipe fifo; do
+
+echo testing "make --jobserver-style=$jobserver_style"
+
+function make() {
+  $make_exe --jobserver-style=$jobserver_style "$@"
+}
+
 make -j1 # 0 workers -> no jobserver
 
 make -j2 # 1 worker
@@ -16,5 +26,7 @@ make -j3 # 2 workers
 make -j4 # 3 workers
 
 make -j4 -l4 # 3 workers + maxLoad 4
+
+done
 
 echo all tests passed
